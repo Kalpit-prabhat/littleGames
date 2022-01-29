1edc9df621418dac8c73908ef23d9ccd0a7d9cfd
Ayush Chaudhary <chaudharyayush4121@gmail.com>
2022-01-29 14:53:09 +0530

    screen resolution changed

--- pong_game.py
+++ pong_game.py
@@ -4,7 +4,7 @@ import turtle
 screen = turtle.Screen()
 screen.title("PONG GAME BY AYUSH CHAUDHARY")
 screen.bgcolor("black")
-screen.setup(width = 1920 , height = 1080)
+screen.setup(width = 1080 , height = 720)
 screen.tracer(0)
 
 
