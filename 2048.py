ddbe14c12684ebe603f45d5eebb53acc20603996
Kugelblitz <prabhatkalpit@gmail.com>
2022-02-02 14:24:29 +0530

    2046 done

--- 2048.py
+++ 2048.py
@@ -1,14 +1,17 @@
 import pygame, random
 
 pygame.init()
-win = pygame.display.set_mode((400, 400))
+win = pygame.display.set_mode((600, 400))
 clock = pygame.time.Clock()
 pygame.display.set_caption('2048')
 
 FPS=60
 TIME = 0.15
+ALPHA = 100
+Highscore = 0
 
 score = 0
+pressed = False
 grid = {(0,0):0,(1,0):0,(2,0):0,(3,0):0,
         (0,1):0,(1,1):0,(2,1):0,(3,1):0,
         (0,2):0,(1,2):0,(2,2):0,(3,2):0,
@@ -33,10 +36,11 @@ class Blocks(object):
         self.rec = pygame.Rect(position,(100,100))
         text = font.render(str(self.val), True, (255,255,255))
         textrect = text.get_rect(center=self.rec.center)
-        value = self.val
-        while value>128:
-            value = value//128
-        pygame.draw.rect(win, colors[value], self.rec,50,10)
+        if self.val>128:
+            color = colors[128]
+        else:
+            color = colors[self.val]
+        pygame.draw.rect(win, color, self.rec,50,10)
         win.blit(text,textrect)
 
 def update(direction):
@@ -87,20 +91,81 @@ def frame_pos(cell,block,frame):
         new = (x,cell[1])
     return new
 
+def game_over():
+    if 0 not in grid.values():
+        for cell, block in grid.items():
+            adjacent = []
+            if cell[0]>0:
+                adjacent.append(grid[(cell[0]-1,cell[1])].val)
+            if cell[0]<3:
+                adjacent.append(grid[(cell[0]+1,cell[1])].val)
+            if cell[1]>0:
+                adjacent.append(grid[(cell[0],cell[1]-1)].val)
+            if cell[1]<3:
+                adjacent.append(grid[(cell[0],cell[1]+1)].val)
+            if block.val in adjacent:
+                return False
+                break
+        else:
+            return True
+
+def restart():
+    global score, grid, score_rect
+    score = 0
+    grid = {(0,0):0,(1,0):0,(2,0):0,(3,0):0,
+        (0,1):0,(1,1):0,(2,1):0,(3,1):0,
+        (0,2):0,(1,2):0,(2,2):0,(3,2):0,
+        (0,3):0,(1,3):0,(2,3):0,(3,3):0,}
+    pygame.draw.rect(win, (0,0,0), score_rect)
+    score_text = font.render(str(score), True, (255, 255, 255))
+    score_rect = score_text.get_rect(center=(500,100))
+    win.blit(score_text,score_rect)
+    win.blit(bg,(0,0))
+    choices = random.sample(grid.keys(),2)
+    for choice in choices:
+        grid[choice] = Blocks(2,choice)
+        grid[choice].draw((choice[0]*100,choice[1]*100))
+
 choices = random.sample(grid.keys(),2)
 for choice in choices:
     grid[choice] = Blocks(2,choice)
 
 font = pygame.font.SysFont('data\\font.ttf',40)
+font2 = pygame.font.SysFont('data\\font.ttf',60)
+bg = pygame.image.load(r'data\2048_bg.png')
+rs = pygame.image.load(r'data\restart.png')
+rs_rect = rs.get_rect()
+rs_rect.center = (500,300)
 frames_needed = int(FPS*TIME)
 
-win.fill((255,255,255))
+text1 = font.render('Score:', True, (255,255,255))
+textrect1 = text1.get_rect(center=(500,50))
+text2 = font.render('Highscore:', True, (255,255,255))
+textrect2 = text2.get_rect(center=(500,150))
+score_text = font.render(str(score), True, (255, 255, 255))
+score_rect = score_text.get_rect(center=(500,100))
+high_text = font.render(str(Highscore), True, (255, 255, 255))
+high_rect = high_text.get_rect(center=(500,200))
+win.blit(text1,textrect1)
+win.blit(text2,textrect2)
+win.blit(score_text,score_rect)
+win.blit(high_text,high_rect)
+win.blit(rs,rs_rect)
+
+win.blit(bg,(0,0))
 for cell, block in grid.items():
     if block:
         block.draw((cell[0]*100,cell[1]*100))
 
 running = True
 while running:
+    pos = pygame.mouse.get_pos()
+    if rs_rect.collidepoint(pos):
+        if pygame.mouse.get_pressed()[0] == 1 and pressed == False:
+            pressed = True
+            restart()
+    if pygame.mouse.get_pressed()[0] == 0 and pressed:
+        pressed = False
     direction = ''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -122,7 +187,7 @@ while running:
         grid = grid_new.copy()
         for frame in range(frames_needed):
             clock.tick(FPS)
-            win.fill((255,255,255))
+            win.blit(bg,(0,0))
             for cell, block in grid.items():
                 if block:
                     if len(block):
@@ -144,6 +209,22 @@ while running:
         rand_pos = random.choice([key for key in grid.keys() if grid[key]==0])
         grid[rand_pos] = Blocks(random.choices([2,4],weights=[10,1])[0],rand_pos)
         grid[rand_pos].draw((rand_pos[0]*100,rand_pos[1]*100))
+        score_text = font.render(str(score), True, (255, 255, 255))
+        score_rect = score_text.get_rect(center=(500,100))
+        pygame.draw.rect(win, (0,0,0), score_rect)
+        win.blit(score_text,score_rect)
+        if game_over():
+            for a in range(ALPHA//4):
+                clock.tick(FPS)
+                s = pygame.Surface((400,400),pygame.SRCALPHA)
+                s.fill((255,255,255,1))
+                win.blit(s,(0,0))
+                pygame.display.flip()
+            text = font2.render('Game Over!', True, (82, 34, 0))
+            textrect = text.get_rect(center=(200,200))
+            win.blit(text,textrect)
+            pygame.display.flip()
+            running = False
     clock.tick(FPS)
     pygame.display.flip()
-pygame.quit()
+
